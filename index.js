42f4521159757a03d015edba0fcc294f2e9d5fa0
Menna <manameno2005yahoo.com@gmail.com>
2025-04-15 19:56:59 +0200

    Active links

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 // Get the current page name from the URL
 let currentPage = window.location.pathname;
 
-// Handle cases where the URL ends with a slash (e.g., https://yourwebsite.com/)
-if (currentPage === '/' || currentPage === '') {
-  currentPage = '/index.html'; // Default to index.html
+// Extract just the file name 
+currentPage = currentPage.substring(currentPage.lastIndexOf('/') + 1);
+
+// If empty, assume it's index.html
+if (currentPage === '') {
+  currentPage = 'index.html';
 }
 
 // Select all navigation links
@@ -18,6 +21,7 @@ navLinks.forEach(link => {
 
 // Loop through each link and add the "active" class if it matches the current page
 navLinks.forEach(link => {
+  const linkPage = link.getAttribute('href');
   if (link.href.includes(currentPage)) {
     link.classList.add('active'); // Add "active" class to the current link
   }
