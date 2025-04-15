a79a2f321ee9ad94dee33351c9448028990f4913
Menna <manameno2005yahoo.com@gmail.com>
2025-04-15 18:12:10 +0200

    NavBar

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // Navigation bar
 
 // Get the current page name from the URL
-let currentPage = window.location.pathname.split("/").pop();
-if (currentPage === "") currentPage = "index.html";
+const currentPage = window.location.pathname;
 
 // Select all navigation links
 const navLinks = document.querySelectorAll('nav a');
 
 // Loop through each link and add the "active" class if it matches the current page
 navLinks.forEach(link => {
-  if (link.getAttribute('href').includes(currentPage)) {
+  if (link.href.includes(`${currentPage}`)) {
     link.classList.add('active'); // Add "active" class to the current link
 
   }
