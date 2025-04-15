bcde9be2b1c93980da81bca8c3212f9589dfd994
Menna <manameno2005yahoo.com@gmail.com>
2025-04-15 19:28:50 +0200

    NavBar

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const currentPage = window.location.pathname;
 // Select all navigation links
 const navLinks = document.querySelectorAll('nav a');
 
+// Reset all links by removing the "active" class
+navLinks.forEach(link => {
+  link.classList.remove('active'); // Remove "active" class from all links
+});
+
 // Loop through each link and add the "active" class if it matches the current page
 navLinks.forEach(link => {
   if (link.href.includes(`${currentPage}`)) {
     link.classList.add('active'); // Add "active" class to the current link
-
   }
 });
