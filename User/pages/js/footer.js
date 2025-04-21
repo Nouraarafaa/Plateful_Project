fb1483995ceb45a73848d1d79ed383afd7f3b3e7
IsRaaAbdelhaq <israaabdellhaq@gmail.com>
2025-04-21 20:53:56 +0200

    edit color all website

---
+++ User/pages/js/footer.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const footerPlaceholder = document.getElementById("footer-placeholder");
+    if (footerPlaceholder) {
+        fetch("/User/pages/html/footer.html")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Failed to load footer");
+                }
+                return response.text();
+            })
+            .then(data => {
+                footerPlaceholder.innerHTML = data;
+            })
+            .catch(error => {
+                console.error("Error loading footer:", error);
+            });
+    }
+});
