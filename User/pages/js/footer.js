087ac397a1eaeb444dfdcf11335c53bbc5b3a0f6
IsRaaAbdelhaq <israaabdellhaq@gmail.com>
2025-04-25 21:35:55 +0300

    fixed footer pathes to work on deploy

--- User/pages/js/footer.js
+++ User/pages/js/footer.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const footerPlaceholder = document.getElementById("footer-placeholder");
     if (footerPlaceholder) {
-        fetch("/User/pages/html/footer.html")
+        fetch("../html/footer.html")
             .then(response => {
                 if (!response.ok) {
                     throw new Error("Failed to load footer");
